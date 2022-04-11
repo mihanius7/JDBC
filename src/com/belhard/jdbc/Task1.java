b7bc470c7b125a95b91c18d4a7e5d723bca99a2c
Mihas Dainiak <mihanius@gmail.com>
2022-04-11 20:26:04 +0300

    First query method

--- src/com/belhard/jdbc/Task1.java
+++ src/com/belhard/jdbc/Task1.java
@@ -1,7 +1,16 @@
 package com.belhard.jdbc;
 
+import com.belhard.jdbc.util.SqlUtil;
+
+import java.sql.SQLException;
+
 public class Task1 {
+
     public static void main(String[] args) {
-        System.out.println("Started");
+        try {
+            System.out.println(SqlUtil.getTableByQuery("title", "film"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/com/belhard/jdbc/util/SqlUtil.java
+++ src/com/belhard/jdbc/util/SqlUtil.java
@@ -1,4 +1,27 @@
 package com.belhard.jdbc.util;
 
+import java.sql.*;
+
 public class SqlUtil {
+    private static final String URL = "localhost:5432";
+    private static final String USER = "postgres";
+    private static final String PASSWD = "root";
+    private static Connection connection;
+
+    static {
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getTableByQuery(String columnName, String tableName) throws SQLException {
+        String queryMask = "SELECT ? FROM ?";
+        PreparedStatement statement = connection.prepareStatement(queryMask);
+        statement.setString(1, columnName);
+        statement.setString(2, tableName);
+        int result = statement.executeUpdate();
+        return null;
+    }
 }
