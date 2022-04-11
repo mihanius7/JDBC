70e958e2a0051e948225b756f0d097536de1b678
Mihas Dainiak <mihanius@gmail.com>
2022-04-11 21:35:08 +0300

    getting films by release year

--- src/com/belhard/jdbc/Task2.java
+++ src/com/belhard/jdbc/Task2.java
@@ -4,11 +4,10 @@ import com.belhard.jdbc.util.SqlUtil;
 
 import java.sql.SQLException;
 
-public class Task1 {
-
+public class Task2 {
     public static void main(String[] args) {
         try {
-            System.out.println(SqlUtil.getFilms(2005, 2007));
+            System.out.println(SqlUtil.getCountries());
         } catch (SQLException e) {
             e.printStackTrace();
         }
