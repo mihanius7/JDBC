e7540772e2176bc12ed87cf2470667932ec4809e
Mihas Dainiak <mihanius@gmail.com>
2022-04-12 09:58:14 +0300

    Lambda used for println()

--- src/com/belhard/jdbc/Task2.java
+++ src/com/belhard/jdbc/Task2.java
@@ -3,12 +3,14 @@ package com.belhard.jdbc;
 import com.belhard.jdbc.util.SqlUtil;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public class Task2 {
     public static void main(String[] args) {
         try {
+            List<SqlUtil.City> cities = SqlUtil.getCities();
             System.out.printf("%24.24s %20.20s\n", "City", "Country");
-            SqlUtil.getCities().forEach(System.out::println);
+            cities.forEach(System.out::println);
         } catch (SQLException e) {
             e.printStackTrace();
         }
